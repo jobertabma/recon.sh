39edad62a69be874639b2cc45c78084826d3ceaa
Max Harley <augeocacher@gmail.com>
2017-11-26 11:47:41 -0500

    Allows modules to resolve with symlinks

--- recon.sh
+++ recon.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-dir=$(dirname $0)
+dir=$(dirname $(realpath $0))
 
 case $1 in
 		"init")
 		$dir/recon-init.sh "$2"
 		;;
 		"track")
-    $dir/recon-track.sh "$2" "$3"
+		$dir/recon-track.sh "$2" "$3"
 		;;
 		"sync")
 		$dir/recon-sync.sh
 		;;
-    "history")
+		"history")
 		$dir/recon-history.sh "$2" "$3"
 		;;
 		"search")
