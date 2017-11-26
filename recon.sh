58310d29f2198a2c48d70d7302d06e669c2ed651
Jobert Abma <jobert@hackerone.com>
2017-11-25 20:56:25 -0800

    eval is evil

--- recon.sh
+++ recon.sh
@@ -4,13 +4,13 @@ dir=$(dirname $0)
 
 case $1 in
 		"init")
-		eval $dir/recon-init.sh $2
+		$dir/recon-init.sh "$2"
 		;;
 		"track")
-		eval $dir/recon-track.sh $2 $3
+    $dir/recon-track.sh "$2" "$3"
 		;;
 		"sync")
-		eval $dir/recon-sync.sh
+		$dir/recon-sync.sh
 		;;
 		*)
 		echo "usage: recon.sh {init|track|sync} [<args>]"
